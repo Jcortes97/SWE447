eeb024fb182b751b5e58eb7f8413f28876747d3f
Jcortes97 <50885510+Jcortes97@users.noreply.github.com>
2021-01-28 15:19:10 -0800

    Update my_Cube.js

--- Common/my_Cube.js
+++ Common/my_Cube.js
@@ -1,14 +1,11 @@
 function my_Cube( vertexShaderId, fragmentShaderId)
 {
-    // initialize with a shader name ID
 
     var myVertShader = vertexShaderId || "myCube-vertex-shader";
     var myFragShader = fragmentShaderId || "myCube-fragment-shader";
 
-    //the 'program' to start initshader
     this.program = initShaders(gl, myVertShader, myFragShader);
 
-    //check for something(?)
     if (this.program < 0) 
     {
         alert("Error: Cube shader pipe failed to compile properly. \n\n" +
@@ -19,22 +16,22 @@ function my_Cube( vertexShaderId, fragmentShaderId)
 
     this.positions = {
         values : new Float32Array([
-            //add MY own list of vertex positions
-            -0.5, -0.5, -0.5, //0, A
+            //vertices
+            -0.7, -0.7, -0.7, //0, A
 
-            -0.5, .5, -0.5, //1, B
+            -0.7, .7, -0.7, //1, B
 
-            -0.5, .5, .5, //2, C
+            -0.7, .7, .7, //2, C
 
-            -0.5, -0.5, .5, //3, D
+            -0.7, -0.7, .7, //3, D
 
-            .5, -0.5, -0.5, //4, E
+            .7, -0.7, -0.7, //4, E
 
-            .5, .5, -0.5, //5, F
+            .7, .7, -0.7, //5, F
 
-            .5, .5, .5, //6, G
+            .7, .7, .7, //6, G
             
-            .5, -0.5, .5 //7, H
+            .7, -0.7, .7 //7, H
 
         ]),
         numComponents : 3
@@ -42,7 +39,7 @@ function my_Cube( vertexShaderId, fragmentShaderId)
 
     this.indicies = {
         values : new Uint16Array([
-            //add my own set of triangle indicies to be created for each face of the cube
+
             0, 7, 3, 0, 4, 7, //side AEHD
 
             4, 6, 7, 4, 5, 6, //side EFGH
@@ -59,12 +56,10 @@ function my_Cube( vertexShaderId, fragmentShaderId)
     };
     this.indicies.count = this.indicies.values.length;
 
-    //buffer creation for the vertex points
     this.positions.buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, this.positions.buffer);
     gl.bufferData(gl.ARRAY_BUFFER, this.positions.values, gl.STATIC_DRAW);
 
-    //buffer creation for the indivies for the CUBE
     this.indicies.buffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indicies.buffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.indicies.values, gl.STATIC_DRAW);
@@ -72,15 +67,12 @@ function my_Cube( vertexShaderId, fragmentShaderId)
     this.positions.attributeLoc = gl.getAttribLocation( this.program, "vPosition");
     gl.enableVertexAttribArray(this.positions.attributeLoc);
 
-    //not sure what this line is for(?)
     MVLoc = gl.getUniformLocation(this.program, "MV");
 
     this.MV = undefined;
 
-    //render function to draw the cube shape
     this.render = function ()
     {
-        //referring to the initshaders provided by WebGL library
         gl.useProgram(this.program);
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this.positions.buffer);
@@ -91,7 +83,6 @@ function my_Cube( vertexShaderId, fragmentShaderId)
 
         gl.uniformMatrix4fv(MVLoc, gl.FALSE, flatten(this.MV));
 
-        //draw cubes base(?)
         gl.drawElements(gl.TRIANGLES, this.indicies.count, gl.UNSIGNED_SHORT, 0);
     }
-}; //end function myCube
+}; 
